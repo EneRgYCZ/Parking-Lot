a91383841291429e8d621d3f7ca8265a0a19b210
EneRgYCZ <32387669+EneRgYCZ@users.noreply.github.com>
2020-02-12 23:20:42 +0200

    Commit test

--- app/src/main/java/com/example/parkinglot/Log_In.java
+++ app/src/main/java/com/example/parkinglot/Log_In.java
@@ -3,14 +3,27 @@ package com.example.parkinglot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Log_In extends AppCompatActivity
 {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log__in);
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/parkinglot/Sign_Up.java
+++ app/src/main/java/com/example/parkinglot/Sign_Up.java
@@ -3,13 +3,20 @@ package com.example.parkinglot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class Sign_Up extends AppCompatActivity {
+public class Sign_Up extends AppCompatActivity
+{
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign__up);
+        btn = (Button)findViewById(R.id.button);
+
+
     }
 }
