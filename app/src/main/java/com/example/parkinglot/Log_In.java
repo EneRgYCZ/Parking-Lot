238b7d22c6185637327e97d052d2450a8036b975
V3ctorZ <stefannoriss@gmail.com>
2020-02-21 14:47:58 +0200

    .

--- app/src/main/java/com/example/parkinglot/Log_In.java
+++ app/src/main/java/com/example/parkinglot/Log_In.java
@@ -6,25 +6,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Log_In extends AppCompatActivity
 {
 
-    Button btn;
+    Button log_in,sign_up;
+
+    EditText username,password;
+
+    Parole_Si_Conturi db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log__in);
-        btn = (Button)findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener()
+        db =  new Parole_Si_Conturi(this);
+        username = findViewById(R.id.editText1);
+        password = findViewById(R.id.editText4);
+        sign_up = findViewById(R.id.button2);
+        log_in = findViewById(R.id.button);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Log_In.this,Sign_Up.class));
+            }
+        });
+        log_in.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                Intent intent = new Intent(Log_In.this, Harta.class);
-                startActivity(intent);
+                String user = username.getText().toString().trim();
+                String pwd = password.getText().toString().trim();
+                Boolean res = db.checkUser(user,pwd);
+                if(res==true)
+                {
+                    Intent intent = new Intent(Log_In.this, Harta.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(Log_In.this,"Login Error",Toast.LENGTH_SHORT);
+                }
             }
         });
     }
--- app/src/main/java/com/example/parkinglot/Sign_Up.java
+++ app/src/main/java/com/example/parkinglot/Sign_Up.java
@@ -2,21 +2,47 @@ package com.example.parkinglot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Sign_Up extends AppCompatActivity
 {
 
     Button btn;
 
+    EditText username, password;
+
+    Parole_Si_Conturi db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign__up);
-        btn = (Button)findViewById(R.id.button);
-
+        db = new Parole_Si_Conturi(this);
+        username = findViewById(R.id.editText2);
+        password = findViewById(R.id.editText3);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString().trim();
+                String pwd = password.getText().toString().trim();
+                long val = db.addUser(user,pwd);
+                if(val>0)
+                {
+                    startActivity(new Intent(Sign_Up.this,Log_In.class));
+                }
+                else
+                {
+                    Toast.makeText(Sign_Up.this,"Registration Error",Toast.LENGTH_SHORT);
+                }
+            }
+        });
 
     }
 }
