602830d83cf0973c3668e18aeb4a206361051ef3
UltraGamingSetup <catalin.zaharia2013@gmail.com>
2020-05-30 16:34:55 +0300

    Final commit

--- app/src/main/java/com/example/parkinglot/Rezervation.java
+++ app/src/main/java/com/example/parkinglot/Rezervation.java
@@ -3,7 +3,9 @@ package com.example.parkinglot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
